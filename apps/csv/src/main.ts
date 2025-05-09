fff6dc31d0bcb3ae040673d1cd07c1f38989a344
Mohammad reza <mo.behzad34@gmail.com>
2025-05-09 23:55:35 +0330

    add kuber

--- apps/csv/src/main.ts
+++ apps/csv/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT');
+
+  await app.listen(port || 3001, () => {
+    console.log(`Server is running on port ${port || 3000}`);
+  });
 }
 bootstrap();
